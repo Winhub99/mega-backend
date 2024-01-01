09bc7f3f9ee85e8e66d308205029bb6e13d49f52
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-01-01 12:10:13 +0530

    added utils

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiResponse{
+    constructor(
+        statusCode,
+        message="Success",
+        data){
+            this.data=data
+            this.message=message
+            this.statusCode= statusCode <400
+        }
+}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+/*const asyncHandler=(callBack)=> async(req,res,next)=>{
+    try{
+        await callBack(req,res,next)
+
+    }catch(error){
+        res.status(err.code || 500).json({
+            success:false,
+            message:err.message
+        })
+    }
+}
+*/
+
+const asyncHandler = (requestHandler)=>{
+    (req,res,next)=> {
+        Promise.resolve(requestHandler(req,res,next))
+        .catch(error=>next(error))
+    }
+}
+export  {asyncHandler}
